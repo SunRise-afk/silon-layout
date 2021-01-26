ad493b4c967e78067fc442043b5ec441daa340f3
Illia Ilin <sunrise.th13@gmail.com>
2021-01-26 15:04:04 +0200

    Updates

---
+++ bundle.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";document.querySelector(".burger").addEventListener("click",(()=>{document.querySelector(".burger_button").classList.toggle("close"),document.querySelector(".burger_nav").classList.toggle("close")}))})();
