21fbd4185698f983ae2a132b3cef6b96affcc158
Illia Ilin <sunrise.th13@gmail.com>
2021-01-26 15:00:33 +0200

    Responsive design added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import "./style.scss";
+
+document.querySelector(".burger").addEventListener("click", () => {
+  document.querySelector(".burger_button").classList.toggle("close");
+  document.querySelector(".burger_nav").classList.toggle("close");
+});
